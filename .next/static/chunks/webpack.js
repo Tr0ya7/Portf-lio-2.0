bafa4446d3266c1269bd088040d9f3c10f01e80f
Tr0ya7 <DevLuizHBarros@proton.me>
2024-11-09 12:28:28 -0300

    New project add

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ba04cc379dc4807e"; }
+/******/ 		__webpack_require__.h = function() { return "5100370f78e7844b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
