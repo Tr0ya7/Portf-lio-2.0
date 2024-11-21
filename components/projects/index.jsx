ef535412c121cdf368672c82955f78b8817f1e7d
Tr0ya7 <DevLuizHBarros@proton.me>
2024-11-20 21:50:05 -0300

    New project page description link

--- components/projects/index.jsx
+++ components/projects/index.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import Title from '../title'
 import ProjectCard from './projectCard'
 import projects from './projects.json'
@@ -8,6 +9,9 @@ export default function Projects() {
             <Title>
                 Alguns Projetos
             </Title>
+            <Link href="https://www.behance.net/luizhedesouz6" className="text-blue-500 underline mb-5 text-2xl">
+                Descrição aprofundada dos projetos (com imagens)
+            </Link>
             {projects.map((project) => 
                 <ProjectCard 
                     key={project.id} title={project.title} names={project.names} href={project.link} 
