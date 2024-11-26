8c01a0fd043df2a226549a0077bd8b58bd85344b
Luiz Henrique <136819845+Tr0ya7@users.noreply.github.com>
2024-11-26 02:06:51 -0300

    Update viewResume.jsx

--- components/contactMe/viewResume.jsx
+++ components/contactMe/viewResume.jsx
@@ -3,7 +3,7 @@ import { IoDocumentOutline } from "react-icons/io5"
 
 export default function ViewResume() {
     return (
-        <Link href="https://drive.google.com/file/d/1F1c-VrIeommnIPeFKRPvHzSx3rJqHI8j/view?usp=sharing">
+        <Link href="https://drive.google.com/file/d/1Vb_iv28E6xOeTKLdEfznD_7reFjqS2Nn/view?usp=drive_link">
             <button 
                 className="
                     flex items-center w-fill p-3 border rounded-md border-white text-white mt-5 mx-auto text-sm 
